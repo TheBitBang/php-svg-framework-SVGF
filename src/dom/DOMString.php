bb904d6e7d0133e4375d2444afd8a2f2dfbe332a
Xavier Atero <xavier.atero@pixel51.net>
2017-06-18 19:52:55 +0200

    Initial commit: src/dom

---
+++ src/dom/DOMString.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace b1t\dom;
+
+/**
+ * DOMString.php
+ *
+ * This class implements the type DOMString as described in Document Object Model (DOM) Level 3 Core Specification.
+ *
+ * It ensures UTF-16 encoding for strings.
+ *
+ * @author	J. Xavier Atero
+ * @link	https://www.w3.org/TR/DOM-Level-3-Core/core.html#DOMString 
+ */
+ 
+class DOMString {
+	 
+	/** @var string $dom_string Representation of a string according to DOM sepecification */
+	private $dom_string;
+	
+	/**
+     * @param string|null $input_string Input string
+     */
+	public function __construct($input_string)
+	{
+		$this->setDOMString($input_string);
+	}
+
+	// set and get methods
+	
+	public function setDOMString($input_string)
+	{
+		$this->dom_string = mb_convert_encoding($input_string, "UTF-16", "auto");
+	}
+
+	public function getDOMString()
+	{
+		return $this->dom_string;
+	}
+
+ }
